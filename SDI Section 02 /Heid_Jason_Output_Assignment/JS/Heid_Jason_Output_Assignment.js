e4148544703cedcbf27f56bf2bd11a4a6801b28b
JTHEID <onehundredand38@gmail.com>
2015-07-09 22:47:20 -0400

    Checking, Double Checking and then some.

---
+++ SDI Section 02 /Heid_Jason_Output_Assignment/JS/Heid_Jason_Output_Assignment.js
@@ -0,0 +1,25 @@
+ /**
+ * Created by JTH1138 on 7/9/15.
+ */
+//Jason Heid
+//SDI Section 02
+//Full Sail University
+//7-9-15
+
+     alert("First assignment!")
+
+
+var favoriteFood = "Lasagna"; //String Value
+
+var likesSushi = true; //Boolean Value
+
+var numberOfChildren = 2; //Integer Value
+
+var dreamCar = "Mercedes Benz G Class"; //String Value 2
+
+var dislikesDogs = false; //Boolean Value 2
+
+var luckyNumber = 138; //Integer Value 2
+
+ console.log(numberOfChildren)
+ 
