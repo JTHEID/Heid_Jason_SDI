566a729c22d53ecd371cdaf2ed8e55be2ddfaae5
JTHEID <onehundredand38@gmail.com>
2015-07-09 21:08:26 -0400

    Screencasts

---
+++ AssociatingJS/js/script.js
@@ -0,0 +1,11 @@
+//Arrays
+
+var ramonesNames = ["Joey", "Johnny", "DeeDee", "Tommy"]; //0,1,2,3
+
+ramonesNames[2] = "CJ";
+
+console.log(ramonesNames[2]);
+ramonesNames[4] = "Marky";
+
+console.log(ramonesNames[4]);
+
